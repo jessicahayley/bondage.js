ea99ae6f787b1cc25696108736a88ebd46230efc
Jessica Hayley <hey@jayhay.me>
2016-06-23 23:29:21 -0400

    Poke a little bit more, have links now

--- src/parser.js
+++ src/parser.js
@@ -3,8 +3,13 @@ const Parser = require('jison').Parser;
 const grammar = {
   lex: {
     rules: [
+      ['\\[\\[', 'return "LINKSTART";'],
+      ['\\]\\]', 'return "LINKEND";'],
       ['\\n', 'return "NEWLINE";'],
       ['$', 'return "EOF";'],
+      ['.+?(?=\\|)', 'return "LINKTEXT";'],
+      ['[A-Za-z0-9]+?(?=\\]\\])', 'return "LINKDEST";'],
+      ['\\|', 'return "|";'],
       ['.*', 'return "TEXT";'],
     ],
   },
@@ -18,7 +23,12 @@ const grammar = {
       ['line', '$$ = [$1];'],
     ],
     line: [
-      ['TEXT', '$$ = $1;'],
+      ['link', '$$ = $1;'],
+      ['TEXT', '$$ = { text: $1, type: "line" };'],
+    ],
+    link: [
+      ['LINKSTART LINKTEXT | LINKDEST LINKEND', '$$ = { text: $2, dest: $4, type: "namedlink" };'],
+      ['LINKSTART LINKDEST LINKEND', '$$ = { dest: $2, type: "link" };'],
     ],
   },
 };
@@ -29,7 +39,7 @@ console.log(parser.parse('HI'))
 console.log(parser.parse('HI\nHI'))
 console.log(parser.parse(' \nHI'))
 console.log(parser.parse('\nHI'))
-// console.log(parser.parse('[[hi]]'));
-// console.log(parser.parse('[[hi|yo]]'));
+console.log(parser.parse('[[hi]]'));
+console.log(parser.parse('[[hi|yo]]'));
 // console.log(parser.parse('abc 123'));
 // console.log(parser.parse('abc123 fcx'));
